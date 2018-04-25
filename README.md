2c1e84b50673fcdcb2ccf948d26433e89eac0d6e
Craig Newton <newtondev@gmail.com>
2018-04-25 13:55:43 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# React's New Context!
+
+Simple little example showing how to create and access state through the new React Context.
---
+++ src/App.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+
+// first we will make a new context
+const MyContext = React.createContext();
+
+// Then create a provider component
+class MyProvider extends Component {
+  state = {
+    name: 'Craig',
+    age: 36,
+    cool: true
+  };
+
+  render() {
+    return (
+      <MyContext.Provider value={{
+        state: this.state,
+        growAYearOlder: () => this.setState({ 
+          age: this.state.age + 1
+        })
+      }}>
+        {this.props.children}
+      </MyContext.Provider>
+    );
+  }
+}
+
+const Family = (props) => (
+  <div className="family">
+    <Person />
+  </div>
+);
+
+class Person extends Component {
+  render() {
+    return (
+      <div className="person">
+        <MyContext.Consumer>
+          {(context) => (
+            <React.Fragment>
+              <p>Age: {context.state.age}</p>
+              <p>Name: {context.state.name}</p>
+              <button onClick={context.growAYearOlder}>Grow</button>
+            </React.Fragment>
+          )}
+        </MyContext.Consumer>
+      </div>
+    );
+  }
+}
+
+class App extends Component {
+  
+
+  render() {
+    return (
+      <MyProvider>
+        <div>
+          <p>I'm the app</p>
+          <Family />
+        </div>
+      </MyProvider>
+    );
+  }
+}
+
+export default App;
